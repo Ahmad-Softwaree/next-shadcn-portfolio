76d79ff084733de9dd4541f0bf9fec1c7af59f07
Ahmad-Software <dr.ahmad.salah.54@gmail.com>
2025-12-27 18:08:01 +0300

    remove log

--- app/projects/[id]/page.tsx
+++ app/projects/[id]/page.tsx
@@ -35,7 +35,6 @@ const page = () => {
     starred,
     types,
   } = project;
-  console.log(image);
   return (
     <section className="py-20 px-6 max-w-4xl mx-auto">
       <div className="space-y-6">
